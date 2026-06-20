3652a5e7e0ee7e342611e3521e5c2ca6e1ba924f
스튜디오보통 <lsn@seutyudiobotong-ui-MacBookAir.local>
2026-06-21 01:02:12 +0900

    네이버 상품이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.pstatic.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "**.phinf.naver.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.phinf.naver.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
